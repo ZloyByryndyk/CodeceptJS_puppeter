c8811c52fb97171f028f880a9e847707095afca6
Rinat Makhmutov <Soolez@mail.ru>
2020-06-02 12:31:42 +0300

    add section "Home page"
    - добавил некоторые настройки в конфиг файл
    - добавил новую секцию с тестами (Home page)

--- _test.js
+++ _test.js
@@ -1,5 +1,51 @@
-Feature('');
+Feature("Home page");
 
-Scenario('test something', (I) => {
+Scenario("EstChange service description", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("EstChange");
+  I.amOnPage("/#about");
+});
 
+Scenario("Get started now", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("Get started now");
+  I.amOnPage("/signup");
+});
+
+Scenario("Start onboarding now", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("Start onboarding now");
+  I.amOnPage("/signup");
+});
+
+Scenario("Privacy Policy", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("Privacy Policy");
+  I.amOnPage("/TOS/2");
+});
+
+Scenario("3rd party KYC system", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("3rd party KYC system");
+  I.amOnPage("https://knowyourcustomer.com/");
+});
+
+Scenario("Blockchain analytics tools", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("blockchain analytics tools");
+  I.amOnPage("https://www.elliptic.co/");
+});
+
+Scenario("Sign in", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("Accept");
+  I.click("Sign in");
+  I.amOnPage("/login/");
+});
+
+Scenario("Sign up", (I) => {
+  I.amOnPage("http://dev.estchange.io/");
+  I.click("Accept");
+  I.click("Sign up");
+  I.amOnPage("/signup");
 });
